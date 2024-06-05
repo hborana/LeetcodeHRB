4e943dc9bfa31c6aa79c5fa39aa4c21171342b9a
Himani Ravindra Borana <139065187+hborana@users.noreply.github.com>
2024-06-05 02:50:50 -0400

    Time: 40 ms (93.31%) | Memory: 16.6 MB (67.21%) - LeetSync

--- 1044-find-common-characters/find-common-characters.py
+++ 1044-find-common-characters/find-common-characters.py
@@ -1,19 +1,16 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        # Initialize a list to store the minimum frequency of each character (a-z)
         min_freq = [float('inf')] * 26
         
-        # Iterate through each word in the list
         for word in words:
-            # Initialize a list to count the frequency of each character in the current word
             char_count = [0] * 26
             for char in word:
                 char_count[ord(char) - ord('a')] += 1
-            # Update the minimum frequency for each character
+    
             for i in range(26):
                 min_freq[i] = min(char_count[i], min_freq[i])
         
-        # Prepare the result list based on the minimum frequencies
+        
         result = []
         for i in range(26):
             result.extend([chr(i + ord('a'))] * min_freq[i])
