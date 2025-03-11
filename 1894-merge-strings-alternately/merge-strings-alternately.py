ec8f33a910bee9c748b8fb9884791979739ec7e4
H_007 <139065187+hborana@users.noreply.github.com>
2025-03-11 00:12:10 -0700

    Time: 38 ms (58.30%) | Memory: 17.9 MB (33.55%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,26 +1,17 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
         merged = []
         i, j = 0, 0
-        n, m = len(word1), len(word2)
+        len1 = len(word1)
+        len2 = len(word2)
 
-        # Alternate between characters of word1 and word2
-        while i < n and j < m:
-            merged.append(word1[i])
-            merged.append(word2[j])
-            i += 1
-            j += 1
-
-        # Append remaining characters from the longer string
-        if i < n:
-            merged.append(word1[i:])
-        if j < m:
-            merged.append(word2[j:])
+        while i < len1 or j < len2:
+            if i < len1:
+                merged.append(word1[i])
+                i += 1
+            if j < len2:
+                merged.append(word2[j])
+                j += 1 
 
         return ''.join(merged)
-            
+        
