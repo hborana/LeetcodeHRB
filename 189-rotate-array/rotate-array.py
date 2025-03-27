d4f9812ed74773ec141545bf5f689b623dee022c
H_007 <139065187+hborana@users.noreply.github.com>
2025-03-27 15:36:14 -0700

    Time: 0 ms (100.00%) | Memory: 25.6 MB (46.13%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -8,12 +8,4 @@ class Solution:
         n = len(nums)
         k %= n
 
-        def reverse(start: int, end: int) -> None:
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
-
-        reverse(0, n - 1)
-        reverse(0, k - 1)
-        reverse(k, n - 1)
+        nums[:] = nums[-k:] + nums[:-k]  # Update the array in place
